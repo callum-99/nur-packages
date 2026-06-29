9b8bd911feae27dba43f615c06fb81e3ae2c3388
Callum Yarnold <git@yarnold.co.uk>
2026-06-29 23:04:22 +0100

    Add grimmory package

---
+++ pkgs/grimmory/default.nix
@@ -0,0 +1,46 @@
+# package.nix
+{ lib, stdenv, fetchurl, makeWrapper, jdk25, ffmpeg }:
+
+let
+  version = "3.2.2";
+
+  jar = fetchurl {
+    url = "https://github.com/grimmory-tools/grimmory/releases/download/v${version}/grimmory.jar";
+    sha256 = ""; # nix-prefetch-url <url> --type sha256
+  };
+
+  kepubifyVersion = "4.0.4";
+  kepubify = fetchurl {
+    url = "https://github.com/pgaskin/kepubify/releases/download/v${kepubifyVersion}/kepubify-linux-${
+      if stdenv.hostPlatform.isAarch64 then "arm64" else "64bit"
+    }";
+    sha256 = if stdenv.hostPlatform.isAarch64
+      then "5a15b8f6f6a96216c69330601bca29638cfee50f7bf48712795cff88ae2d03a3"
+      else "37d7628d26c5c906f607f24b36f781f306075e7073a6fe7820a751bb60431fc5";
+  };
+in
+stdenv.mkDerivation {
+  pname = "grimmory";
+  inherit version;
+  dontUnpack = true;
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin $out/share/java $out/libexec
+    cp ${jar} $out/share/java/grimmory.jar
+    install -m755 ${kepubify} $out/libexec/kepubify
+
+    makeWrapper ${jdk25}/bin/java $out/bin/grimmory \
+      --add-flags "-jar $out/share/java/grimmory.jar" \
+      --prefix PATH : ${lib.makeBinPath [ ffmpeg ]} \
+      --set KEPUBIFY_PATH $out/libexec/kepubify
+  '';
+
+  meta = with lib; {
+    description = "Self-hosted digital library, fork of Booklore";
+    homepage = "https://github.com/grimmory-tools/grimmory";
+    license = licenses.agpl3Plus;
+    mainProgram = "grimmory";
+    platforms = platforms.linux;
+  };
+}
